7d9538ee339d630afb205fb3a40c89cd3aafe1e0
graceDev1 <grace.devolop@gmail.com>
2021-02-04 16:05:04 +0200

    last changer to my app

--- source/main.js
+++ source/main.js
@@ -1,13 +1,23 @@
 import express from 'express';
 import {graphqlHTTP} from 'express-graphql';
 import schema from './schema';
+import cors from 'cors';
+import path from 'path';
 
 const app = express()
+
+app.use(cors())
 
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
-}))
+}));
+
+app.use(express.static('public'));
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'public', 'index.html'));
+})
 
 const port = process.env.PORT || 4000;
 
--- source/schema.js
+++ source/schema.js
@@ -15,7 +15,7 @@ const LaunchType = new GraphQLObjectType({
         flight_number: {type: GraphQLInt},
         mission_name: {type: GraphQLString},
         launch_year: {type: GraphQLString},
-        launch_data_local: {type: GraphQLString},
+        launch_date_local: {type: GraphQLString},
         launch_success: {type: GraphQLBoolean},
         rocket: {type: RocketType}
     })
@@ -36,7 +36,7 @@ const RocketType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
     name:'RootQueryType',
     fields:{
-        launces: {
+        launches: {
             type: new GraphQLList(LaunchType),
             resolve(parent, args){
                 return axios.get('https://api.spacexdata.com/v3/launches')
